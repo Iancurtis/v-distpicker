8f7583630e93bae4d513bbb5439e77566e5f3fe4
Fyn <fyn@bohaisoft.cn>
2017-06-08 19:22:41 +0800

    feat(vue): for Vue.use

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import VDistpicker from './Distpicker'
 
+
+function install(Vue) {
+    Vue.component('VDistpicker', VDistpicker)
+}
+export {
+    VDistpicker,
+    install
+}
 export default VDistpicker
